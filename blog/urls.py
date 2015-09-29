58be13e1cd0651d254ec7ac73bc013c1993d5f4a
joel ivongbe <engrjoelivon@yahoo.com>
2015-09-29 17:06:13 -0500

    included a json view

--- blog/urls.py
+++ blog/urls.py
@@ -2,4 +2,7 @@ from django.conf.urls import include, url,patterns
 from blog import views
 
 
-urlpatterns=patterns('',url(r'^$',views.home,name="home"),)
+
+urlpatterns=patterns('',url(r'^$',views.home,name="home"),
+                     url(r'^json/$',views.ret_json,name="ret_json")
+                     )
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
 
 # Create your views here.
 def home(request):
     return  HttpResponse("welcome to my page")
+
+
+def ret_json(request):
+    data = {'foo': 'bar', 'hello': 'world'}
+    return HttpResponse(json.dumps(data), content_type='application/json')
